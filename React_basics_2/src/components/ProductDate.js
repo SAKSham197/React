3bddb9346d57771def80b02d155e49f7b977d65c
Saksham Pandey <sakshampandey9695@gmail.com>
2025-07-19 22:47:42 +0530

    Locale used instead of Local in toLocaleString

--- React_basics_2/src/components/ProductDate.js
+++ React_basics_2/src/components/ProductDate.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './ProductDate.css'
 
 const ProductDate = (props) => {
-    const month=props.date.toLocalString('en-US',{month:'long'})
-    const day=props.date.toLocalString('en-US',{day:'2-digit'})
-    const year=props.date.getFullYear()
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
     </div>
   )
 }
