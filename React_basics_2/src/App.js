ae2c232c37137547c4fff5ec2e93161caa33f76b
Saksham Pandey <sakshampandey9695@gmail.com>
2025-07-27 21:47:26 +0530

    App.js changes

--- React_basics_2/src/App.js
+++ React_basics_2/src/App.js
@@ -1,42 +1,45 @@
 import React from 'react';
-import './App.css';
-import Products from './components/Products';
 import NewProduct from './components/NewProduct';
 
+import Products from './components/Products';
 
-function App() {
+const App = () => {
   const products = [
     {
-      id:'p1',
-      title:'Prod1',
-      amount:100,
-      date:new Date(2021,8,10)
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
     },
-    {
-      id: 'p2',
-      title: 'Prod2',
-      amount: 150,
-      date: new Date(2021, 9, 12)
-    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
     {
       id: 'p3',
-      title: 'Prod3',
-      amount: 200,
-      date: new Date(2021, 10, 5)
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
     },
     {
       id: 'p4',
-      title: 'Prod4',
-      amount: 75,
-      date: new Date(2021, 11, 20)
-    }  
-  ]
-  return (
-    <div className="App">
-      <NewProduct/>
-      <Products items={products}></Products>
-    </div>
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
 
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <NewProduct pranay = {printProductData} />
+      <Products items={products} />
+    </div>
   );
 }
 
