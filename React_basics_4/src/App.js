ffbd0a130a75a9ded96b0a1c7fb50ae0c755e6b3
Saksham Pandey <sakshampandey9695@gmail.com>
2025-08-10 12:28:22 +0530

    Folder name changes

--- React_basics_4/src/App.js
+++ React_basics_4/src/App.js
@@ -1,64 +1,30 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Filter from "./Components/Filter";
-import Cards from "./Components/Cards";
-import Spinner from "./Components/Spinner";
-import { apiUrl, filterData } from "./data.js";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import data from './data.js';
+import Tours from './Component/Tours';
 
 const App = () => {
-  // const [courses, setCourses] = useState(null);
-  const [courses, setCourses] = useState([]);
-  const [laoding, setLoading] = useState(true);
-  const [category, setCategory] = useState([]);
+  const [tours, setTours] = useState(data);
+  
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
 
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const res = await fetch(apiUrl);
-      const output = await res.json();
-
-      // Save data
-      setCourses(output.data);
-      // setCourses(output);
-    } catch (err) {
-      toast.error("Something Went Wrong");
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btnWhite" onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    );
+  }
 
   return (
-    <div className="min-h-screen flex-col flex bg-bgDark2">
-      <div>
-        <Navbar />
-      </div>
-
-      <div className="bg-bgDark2">
-        <div>
-          <Filter
-            filterData={filterData}
-            category={category}
-            setCategory={setCategory}
-          />
-        </div>
-
-        <div className="w-11/12 max-w-[1200px] min-h-[50vh] mx-auto flex flex-wrap justify-center items-center">
-          {
-            laoding ? (
-              <Spinner />
-            ) : (
-              <Cards courses={courses} category={category} />
-            )
-          }
-        </div>
-      </div>
+    <div className="app">
+      <Tours tours={tours} removeTours={removeTour} />
     </div>
-  );
+  )
+
 };
 
 export default App;
--- React_basics_4/src/index.js
+++ React_basics_4/src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <App />
-    <ToastContainer />
-  </>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
